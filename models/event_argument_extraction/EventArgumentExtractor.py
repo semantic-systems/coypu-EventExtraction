3bdccb1578b69ba7789ffa25a87e548d988a1b2a
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-03-21 12:47:22 +0000

    docu: added documentation for xixi.

--- models/event_argument_extraction/EventArgumentExtractor.py
+++ models/event_argument_extraction/EventArgumentExtractor.py
@@ -4,6 +4,8 @@ from schemes import EventArgumentExtractorOutput
 class OpenIEExtractor(object):
     def __init__(self):
         self.name = self.__name__
+        ## instantiate your extractor here
 
     def forward(self, tweet: str) -> EventArgumentExtractorOutput:
+        ## call your extractor here, which takes as input a tweet, and stores output as type EventArgumentExtractorOutput
         raise NotImplementedError
