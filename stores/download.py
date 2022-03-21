91cfce8881a714409ab3acc9db102596e35ab515
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-03-21 14:59:14 +0000

    fix: download script

--- stores/download.py
+++ stores/download.py
@@ -1,6 +1,13 @@
 import gdown
+from pathlib import Path
 
 
 def download_from_google_drive(url: str, output: str):
-    gdown.download(url, output)
+    gdown.download(url, output, fuzzy=True)
     print(f"{url} saved to {output}")
+
+
+if __name__ == '__main__':
+    url = "https://drive.google.com/file/d/18vOZP0kT9XRVxkC-C5fQp0JiirY5sRFl/view?usp=sharing"
+    output = str(Path(__file__).parent.joinpath("models").absolute()) + "pretrained_event_detector.pt"
+    download_from_google_drive(url, output)
