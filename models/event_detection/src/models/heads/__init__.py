456304cdb1ca91211877bc9188f61cfc5dd56c42
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-03-24 11:15:09 +0000

    fix: remove src/ from gitignore.

---
+++ models/event_detection/src/models/heads/__init__.py
@@ -0,0 +1,3 @@
+from models.event_detection.src.models.heads.head import Head
+from models.event_detection.src.models.heads.linear_layer_head import LinearLayerHead
+from models.event_detection.src.models.heads.prototypical_head import PrototypicalHead
