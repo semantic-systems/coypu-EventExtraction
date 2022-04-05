6f85c4dc7dec718b364048d06820e4639fd7d32a
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-04-05 17:44:06 +0200

    config: added configuration in yml and its dataclass for validation.

--- schemes.py
+++ schemes.py
@@ -1,5 +1,24 @@
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Union
 from dataclasses import dataclass
+from stores.download import LocalMetaConfig
+
+
+@dataclass
+class PublicMetaConfig:
+    package: str
+
+
+@dataclass
+class ModelConfig:
+    type: str
+    meta: Union[LocalMetaConfig, PublicMetaConfig]
+
+
+@dataclass
+class Config:
+    event_type_detector: ModelConfig
+    event_argument_extractor: ModelConfig
+    event_extractor: Optional[ModelConfig]
 
 
 @dataclass
--- stores/download.py
+++ stores/download.py
@@ -1,12 +1,42 @@
 import gdown
+import argparse
 from pathlib import Path
+from dataclasses import dataclass
 
 
-def download_from_google_drive(url: str, output: str):
-    gdown.download(url, output, fuzzy=True)
+@dataclass
+class LocalMetaConfig:
+    google_drive_link: str
+    directory_to_store: str
+
+
+def parse():
+    parser = argparse.ArgumentParser(description='Please input the google drive link to download the model checkpoint.'
+                                                 'As well as where the model should be stored. ')
+
+    parser.add_argument("-link", "--link",
+                        help='Google Drive link to download the model checkpoint.',
+                        default="https://drive.google.com/file/d/1cZd9dxValoqwy_85ZTQMtnZW7m1mJ1wQ/view?usp=sharing")
+    parser.add_argument("-path", "--path",
+                        help='Path to a directory where the model checkpoint is saved.'
+                             'This should NOT be located within this repo because we want to store big files withinin '
+                             '/data',
+                        default="../data")
+    parser.parse_args()
+    return parser.parse_args()
+
+
+def download_from_google_drive(config: LocalMetaConfig):
+    if not Path(config.directory_to_store).exists():
+        Path(config.directory_to_store).mkdir(parents=True, exist_ok=False)
+    gdown.download(config.google_drive_link,
+                   str(Path(config.directory_to_store).joinpath("pretrained_event_detector.pt").absolute()),
+                   fuzzy=True)
 
 
 if __name__ == '__main__':
-    url = "https://drive.google.com/file/d/1cZd9dxValoqwy_85ZTQMtnZW7m1mJ1wQ/view?usp=sharing"
-    output = str(Path(__file__).parent.joinpath("models").absolute()) + "/pretrained_event_detector.pt"
-    download_from_google_drive(url, output)
+    args = parse()
+    google_drive_link = args.link
+    directory_to_store = args.path
+    config = LocalMetaConfig(google_drive_link=google_drive_link, directory_to_store=directory_to_store)
+    download_from_google_drive(config)
