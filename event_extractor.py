8cd2bb3a70b704c5cc370885e4aafa68a7e4f2ba
Junbo Huang <junbo.huang@uni-hamburg.de>
2022-11-23 14:37:06 +0100

    :sparkle: prettify demo

--- event_extractor.py
+++ event_extractor.py
@@ -44,12 +44,27 @@ if __name__ == '__main__':
     event_argument_extractor = OpenTapiocaArgumentExtractor()
     event_extractor = EventExtractor(event_detector=event_detector, event_argument_extractor=event_argument_extractor)
     import gradio as gr
+
+    title = "Hitec Event Extraction Demo v2"
+    description = "Event Detector: crisis-related LM + supervised contrastive learning on TREC-IS dataset. \n\n " \
+                  "Event Argument Extractor: generic knowledge extraction module (open tapioca).\n\n " \
+                  "(paste the extracted event graph [here](https://json-ld.org/playground) to see the visualization!)"
+    article = '<img src="https://lh5.googleusercontent.com/5BFOPuJcVnIYF4P5bmvxTAYoD2cLsqFa_FZUyyUqYCivGq3XdfEPQJ_EvOMdwHD84957qvcitITbAMlKZi5E1nTBjrLXeG7rxPl2FbjJ_I8Ka7gnn7lX7ce1ZZUMlfzjn5R9hztf_q2K3T_lWMDki6AdtFuHT-YeikcG0j8QXCeWzONg6-t3e4NC4gmF" width=150px><img src="https://lh5.googleusercontent.com/vq_hxMF8lqmXdxIQJJGwq9SPRrE15SFRwCWHBWZXlFS_10LTAuzHmcx-1NUYznbXwH02KZgjpwz5eYM-j5m3RUOhW90rDV9uFj-30dBg7kS6irstL3_VHuHkOe5jskDM2-rTLAKhchDlrH3hAG8W9o9XIlzxWD-tQoj83oyrv6NVmsjhmAPR6ohDsmED" width=120px>'
+    with gr.Blocks() as d:
+        with gr.Row():
+            input_box = gr.Textbox(placeholder="Enter a sentence here...", label="Input Tweet")
+            output_box_event_type = gr.Textbox(label="Event type:")
+            output_box_entities = gr.JSON(label="Extracted entities:")
+            output_box_graph = gr.JSON(label="Event graph:")
     demo = gr.Interface(fn=event_extractor.infer,
-                        inputs=gr.Textbox(placeholder="Enter a sentence here..."),
-                        outputs=["text", "json", "json"],
+                        inputs=input_box,
+                        outputs= [output_box_event_type, output_box_entities, output_box_graph],
                         examples=[["A preliminary 6.20 magnitude #earthquake has occurred near Taft, Eastern Visayas, #Philippines."],
                                    ["A shooting has been reported at Saugus High School in Santa Clarita just north of Los Angeles."],
                                    ["Six Vicpol officers have tested positive this month #COVID19"],
                                    ["One person was missing following a large explosion at an apparent industrial building in Houston Friday. The blast damaged nearby buildings and homes."]
-                                   ])
+                                   ],
+                        title=title,
+                        description=description,
+                        article=article)
     demo.launch(share=True, show_error=True)
