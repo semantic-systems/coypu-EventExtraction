cdc223f6e34bf13fd3160dadd0c14dbc836980d1
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2023-02-15 11:10:46 +0100

    Remove hard-coded paths in event detector

--- models/event_detection/EventDetector.py
+++ models/event_detection/EventDetector.py
@@ -15,6 +15,8 @@ logging.set_verbosity_error()
 
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
+DEFAULT_OUTPUT_PATH = 'data/event_detector/'
+DEFAULT_LANGUAGE_MODELS_PATH = 'data/language_models/'
 
 
 @dataclass
@@ -33,12 +35,16 @@ class SingleLabelClassificationForwardOutput:
 
 
 class EventDetector(BaseComponent):
-    def __init__(self, path_to_pretrained_model: str = "/data/event_detector/crisisbert_w_oos_linear.pt"):
+    def __init__(
+            self,
+            path_to_pretrained_model: str = os.path.join(DEFAULT_OUTPUT_PATH, "crisisbert_w_oos_linear.pt")
+    ):
         super(EventDetector).__init__()
         self.prepare(path_to_pretrained_model)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         checkpoint = torch.load(path_to_pretrained_model, map_location=self.device)
-        checkpoint['config']['model']["from_pretrained"] = "/data/language_models/CoyPu-CrisisLM-v1"
+        checkpoint['config']['model']["from_pretrained"] = \
+            os.path.join(DEFAULT_LANGUAGE_MODELS_PATH, "CoyPu-CrisisLM-v1")
         self.model = SingleLabelSequenceClassification(checkpoint['config'])
         self.model.load_state_dict(checkpoint['model_state_dict'])
         self.index_label_map = checkpoint['index_label_map']
@@ -63,13 +69,14 @@ class EventDetector(BaseComponent):
     @staticmethod
     def prepare(path_to_pretrained_model):
         if not Path(path_to_pretrained_model).exists():
-            if not Path("/data/event_detector").exists():
-                Path("/data/event_detector").mkdir()
+            if not Path(DEFAULT_OUTPUT_PATH).exists():
+                Path(DEFAULT_OUTPUT_PATH).mkdir()
             gdown.download(url="https://drive.google.com/file/d/1Hj_s7UfKYOMszQYAYLy0iNFN1qD1wxrH/view?usp=sharing&confirm=t",
                            output=path_to_pretrained_model, fuzzy=True)
-        if not Path("/data/language_models/CoyPu-CrisisLM-v1").exists():
-            if not Path("/data/language_models/").exists():
-                Path("/data/language_models/").mkdir()
+        path_str = os.path.join(DEFAULT_LANGUAGE_MODELS_PATH, "CoyPu-CrisisLM-v1")
+        if not Path(path_str).exists():
+            if not Path(DEFAULT_LANGUAGE_MODELS_PATH).exists():
+                Path(DEFAULT_LANGUAGE_MODELS_PATH).mkdir()
             gdown.download_folder(
                 url="https://drive.google.com/drive/folders/1u6Mthkr4ffVNSjPn3F_B49axTsCHwRv8?usp=sharing&confirm=t",
-                output="/data/language_models/CoyPu-CrisisLM-v1")
+                output=path_str)
