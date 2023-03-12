0a7335869251589dea69edc82d3ba587e2c753ca
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-03-12 18:48:48 +0100

    :bug: added gdelt call here instead of the frontend repo

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from flask import abort, Flask, jsonify, request
 from flask_healthz import healthz
-from models.event_detection.EventDetector import EventDetector
+from models.event_detection.EventDetector import EventDetector, GdeltFunctions
 
 app = Flask(__name__)
 
@@ -22,20 +22,22 @@ app.config.update(
     }
 )
 event_detector = EventDetector()
-
+api = GdeltFunctions()
 
 @app.route('/', methods=['POST'])
 def flask():
     if not request.json or not 'message' in request.json:
         print(request.json)
         abort(400)
-
+    descriptions = ""
     message = request.json['message']
-
+    sentences, description = api.get_feed(message)
+    descriptions += description
     fig_cls, fig_cluster, description = event_detector.forward_batch(message)
+    descriptions += description
 
     response = {'message': message, 'fig_cls': fig_cls, 'fig_cluster': fig_cluster,
-                'descriptions': description}
+                'descriptions': descriptions}
     return jsonify(response), 200
 
 
